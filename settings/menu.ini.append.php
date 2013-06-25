685da876e3e056536da8efb0af5728e09f826013
Nicolas AGUENOT <contact@nicolasaguenot.com>
2013-06-25 12:36:51 +0300

    Create menu.ini.append.php

---
+++ settings/menu.ini.append.php
@@ -0,0 +1,24 @@
+<?php /*
+
+[NavigationPart]
+Part[translatenavigationpart]=Translation
+ 
+[TopAdminMenu]
+Tabs[]=translate
+ 
+[Topmenu_translate]
+NavigationPartIdentifier=translatenavigationpart
+Name=Translation
+URL[]
+URL[default]=translate/list
+Enabled[]
+Enabled[default]=true
+Enabled[browse]=false
+Enabled[edit]=false
+Shown[]
+Shown[default]=true
+Shown[edit]=true
+Shown[navigation]=true
+Shown[browse]=true
+
+*/ ?>
