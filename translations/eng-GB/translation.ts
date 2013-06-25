d67a8f673eb50332a8faf9dee7208303a6d0075b
Nicolas AGUENOT <contact@nicolasaguenot.com>
2013-06-25 12:38:44 +0300

    Create translation.ts

---
+++ translations/eng-GB/translation.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>natranslate</name>
+
+    <message>
+      <source>Generation</source>
+      <translation>Génération</translation>
+    </message>
+    <message>
+      <source>Liste</source>
+      <translation>List</translation>
+    </message>
+    <message>
+      <source>Generation files translation</source>
+      <translation>Générer les fichiers de traduction</translation>
+    </message>
+    <message>
+      <source>The generation passed well</source>
+      <translation>La génération s'est effectuée correctement</translation>
+    </message>
+    <message>
+      <source>Go to translation list</source>
+      <translation>Aller à la liste des traductions</translation>
+    </message>
+    <message>
+      <source>A problem arose during the files generation</source>
+      <translation>Un problème a eu lieu lors de la génération des fichiers.</translation>
+    </message>
+    <message>
+      <source>Validate generation</source>
+      <translation>Valider la génération</translation>
+    </message>
+    <message>
+      <source>Send generation</source>
+      <translation>Lancer la génération</translation>
+    </message>
+    <message>
+      <source>Cancel generation</source>
+      <translation>Annuler la génération</translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the generation?</source>
+      <translation>Voulez-vous vraiment annuler la génération ?</translation>
+    </message>
+    <message>
+      <source>Choose the extensions to be crossed for the generation</source>
+      <translation>Choisissez les extensions à prendre en compte pour la génération des fichiers.</translation>
+    </message>
+    <message>
+      <source>Choose context</source>
+      <translation>Choisir un contexte</translation>
+    </message>
+    <message>
+      <source>Choose language</source>
+      <translation>Choisir une langue</translation>
+    </message>
+    <message>
+      <source>Search translation name</source>
+      <translation>Rechercher une clé de traduction</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Search</translation>
+    </message>
+    <message>
+      <source>Reset</source>
+      <translation>Réinitialiser</translation>
+    </message>
+    <message>
+      <source>This extension aims at generating a file of translation by language, and at being able to modify them in the interface of the backOffice of eZ.</source>
+      <translation>Cette extension a pour but de générer un fichier de traduction par langue, et de fournir une interface permettant de modifier ces traductions dans le backOffice eZPublish.</translation>
+    </message>
+    <message>
+      <source>Before anything else, you have to generate the files of translations to be homogeneous.</source>
+      <translation>Avant toutes choses, il est nécessaire de générer les fichiers de traductions.</translation>
+    </message>
+    <message>
+      <source>Translator / List</source>
+      <translation>Traducteur / Liste</translation>
+    </message>
+    <message>
+      <source>translations</source>
+      <translation>traductions</translation>
+    </message>
+    <message>
+      <source>Translation name</source>
+      <translation>Clé de traduction</translation>
+    </message>
+    <message>
+      <source>Context name Translation</source>
+      <translation>Contexte de traduction</translation>
+    </message>
+      <message>
+      <source>Click to edit</source>
+      <translation>Clic</translation>
+    </message>
+    <message>
+      <source>Click to open</source>
+      <translation>Cliquer pour ouvrir</translation>
+    </message>
+    <message>
+      <source>Validate translation</source>
+      <translation>Valider la traduction</translation>
+    </message>
+    <message>
+      <source>Send traduction</source>
+      <translation>Envoyer la traduction</translation>
+    </message>
+    <message>
+      <source>Cancel translation</source>
+      <translation>Annuler la traduction</translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the translation?</source>
+      <translation>Voulez-vous vraiment annuler la traduction ?</translation>
+    </message>
+    <message>
+      <source>Edit translation</source>
+      <translation>Éditer la traduction</translation>
+    </message>
+  <message><source>List</source><translation>Liste</translation></message><message><source>Search translation key</source><translation>Rechercher une clé de traduction</translation></message><message><source>Translation key</source><translation>Clé de traduction</translation></message><message><source>Translator / Search</source><translation>Traducteur / Recherche</translation></message><message><source>Click to edit</source><translation></translation></message><message><source>Generate files</source><translation>Generate files translation</translation></message><message><source>Translation</source><translation></translation></message><message><source>Notice</source><translation></translation></message><message><source>Translate / List</source><translation></translation></message><message><source>Translate / Notice</source><translation></translation></message><message><source>Before to begin, please check the %1 notice %2</source><translation></translation></message></context><context><name>natranslate/notice</name><message><source>This extension aims at generate and manage files translations</source><translation></translation></message><message><source>To avoid to have a (very) big list of translation, just important translation, related of your website is present in this list</source><translation></translation></message><message><source>If, for any reason, you don't find your text line in this list, please contact your webmaster or favourite admin</source><translation></translation></message><message><source>Usability note</source><translation></translation></message><message><source>Presentation</source><translation></translation></message><message><source>Click on the tab %1 Translation %2</source><translation></translation></message><message><source>When this webpage is loaded, you've</source><translation></translation></message><message><source>At the left : 3 links to go to...</source><translation></translation></message><message><source>the list of translation (your default view)</source><translation></translation></message><message><source>the generation of files</source><translation></translation></message><message><source>this notice</source><translation></translation></message><message><source>At the center : Your translation list view</source><translation></translation></message><message><source>Translation edition</source><translation></translation></message><message><source>You can edit your translation with the right button %1 Click to edit %2</source><translation></translation></message><message><source>When you click on this button, a edition page is display with as many fields as related language in admin siteaccess / SiteLanguageList tab</source><translation></translation></message><message><source>If you want to add a brand new language, please contact the webmaster or favourite admin</source><translation></translation></message><message><source>When your translation edition is finished, click on %1 Send %2 button</source><translation></translation></message><message><source>Your webpage reload and display previous list of translation</source><translation></translation></message><message><source>Important note about translation</source><translation></translation></message><message><source>When you see the list of translation content, you can see [Param]x</source><translation></translation></message><message><source>These info are very important because there is params use for translate elements</source><translation></translation></message><message><source>For example</source><translation></translation></message><message><source>If you've %1 Training for [Param]1 %2, you edit your content and have %1 Training for %3 %2</source><translation></translation></message><message><source>is an important value and must be inserted in your others translations block ( %1 Formation pour %3 %2 for French block for example)</source><translation></translation></message><message><source>I can't edit my translation in a desired language</source><translation></translation></message><message><source>If you don't have a block to edit in the desired language, you can contact the webmaster or favourite admin to unlocked you.</source><translation></translation></message><message><source>Maybe it's a problem linked to your configuration and a reload of your configuration file is concievable.</source><translation></translation></message></context></TS>
